7c900359b6d51197363bbc1c2fe7065b5427f942
TheJeffi <TheJeffI@yandex.ru>
2022-04-17 09:58:16 +0300

    temp

---
+++ src/redux/state.js
@@ -0,0 +1,21 @@
+let state = {
+   posts: [
+      { id: 1,message: "привет, как дела?", likesCount: 14},
+       {id: 2, message: "hello world", likesCount: 12},
+       {id: 3, message: "hello world", likesCount: 12},
+    ],
+    dialogs: [
+      { id: 1, name: "Богдан" },
+      { id: 2, name: "Ильсаф" },
+      { id: 3, name: "Саша" },
+      { id: 4, name: "Настя" },
+    ],
+    
+    messages: [
+      { id: 1, message: "ку как дела" },
+      { id: 2, message: "ку че делаешь" },
+      { id: 3, message: "ку ку" },
+    ],
+}
+
+export default state;
